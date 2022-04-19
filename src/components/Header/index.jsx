35edeaf5c373a58a2c7511943f8f2b2a0c46cfd9
Loïs <flojuve@hotmail.fr>
2022-04-19 14:04:43 +0200

    init pages + routes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return <Link to="/">Accueil</Link>;
+}
+
+export default Header;
---
+++ src/pages/About/About.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <p>Des trucs sur nous</p>
+    </div>
+  );
+}
+
+export default About;
